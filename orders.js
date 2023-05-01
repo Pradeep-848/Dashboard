c1f29f0f92ecca8f3f20dca4568e5d476ec63f60
Pradeep M <125256248+Pradeep-848@users.noreply.github.com>
2023-05-01 18:56:25 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const orders =[
+ {
+  productName:'Tesla tech Drone',
+  productNumber: '35431',
+  paymentStatus: 'Paid',
+  shipping: 'Delivered'
+ },
+{
+  productName: 'Machismo Ap007 Drone',
+  productNumber: '23878',
+  paymentStatus: 'Refunded',
+  shipping: 'Declined'
+ },
+ {
+  productName: 'Amazon Pro Drone',
+  productNumber: '49347',
+  paymentStatus: 'Due',
+  shipping: 'Pending'
+ },
+ {
+  productName: 'Giraffe Drone',
+  productNumber: '78908',
+  paymentStatus: 'Paid',
+  shipping: 'Delivered'
+ },
+ {
+  productName: 'GPS 4k Drone',
+  productNumber: '22821',
+  paymentStatus: 'Due',
+  shipping:'Pending'
+ },
+ {
+  productName: 'DJI Air 2S',
+  productNumber: '81475',
+  paymentStatus:'Refunded',
+  shipping: 'Declined'
+ },
+ {
+  productName: 'Hayabusa Drone',
+  productNumber: '00482',
+  paymentStatus: 'Paid',
+  shipping: 'Delivered'
+ }
+]
